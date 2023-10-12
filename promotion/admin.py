e23b0f892176983a6ef40234a0865376fcb04846
yfjelley <9162896+yfjelley@user.noreply.gitee.com>
2023-10-12 23:25:38 +0800

    add comment

--- promotion/admin.py
+++ promotion/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'content', 'created_at')
+    search_fields = ['name', 'email', 'content']
+
+admin.site.register(Message, MessageAdmin)
--- promotion/views.py
+++ promotion/views.py
@@ -7,3 +7,25 @@ from django.http import HttpResponse
 def index(request):
     context = {}
     return render(request, "promotion/index.html", context)
+
+
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from .models import Message
+
+
+@csrf_exempt
+@require_POST
+def save_message(request):
+    try:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message_content = request.POST.get('message')
+
+        message = Message(name=name, email=email, content=message_content)
+        message.save()
+
+        return JsonResponse({'status': 'success'}, status=201)
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'error': str(e)}, status=400)
